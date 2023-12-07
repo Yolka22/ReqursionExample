03775c20e481a78222460a773ff3cfa5a62c2db9
Yehor <egorkorsun7@gmail.com>
2023-12-07 16:19:13 +0100

    Добавьте файлы проекта.

---
+++ Reqursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reqursion
+{
+    internal class Program
+    {
+
+        static int Reqursion(int iterator)
+        {
+            if (iterator == 0)
+            {
+                Console.WriteLine(iterator);
+                return 0;
+            }
+            Console.WriteLine(iterator);
+            return Reqursion(iterator - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Reqursion(0); // 0 iterations
+            Reqursion(10);// 10-N iterations
+            Reqursion(-2);// infinity loop
+
+            Console.ReadLine();
+        }
+    }
+}
